2094fc1e33332700eb5f950800e9a0cbdba33139
jrbarona4 <149226104+jrbarona4@users.noreply.github.com>
2026-03-16 23:57:47 +0800

    Add addEventListener to script.js

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,20 @@
 let studentName;
 
+const form = document.querySelector('form');
+
+form.addEventListener('btn', (e) => {
+    e.preventDefault();
+
+    document.querySelectorAll('[type="checkbox"]').forEach(item => {
+        
+        let words = [];
+        if (item.checked === true) {
+            words.push(item.value);
+        }
+    })
+    console.log(words);
+})
+
 document.getElementById("btn").onclick = function(){
     studentName = document.getElementById("student-name").value;
     document.getElementById("feedback").textContent = 
